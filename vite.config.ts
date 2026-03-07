ec2371a2fe823e47e4b48339e4e3b6fef7a7f543
Julian Herzfeld <herzfeldjulian@hotmail.de>
2026-03-07 03:13:22 +0100

    first commit, basic structure

---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: "/memory-project/"               //hier angeben wo was projekt liegt. Unterordner anpassen! -> standard ist root mit “/”
+});
